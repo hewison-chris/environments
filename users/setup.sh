e5753af15401c303230f701c2259248a515c04d6
Geod24 <pro.mathias.lang@gmail.com>
2020-09-23 12:39:03 +0900

    users/setup.sh: Improve and fix

--- users/setup.sh
+++ users/setup.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-set -xeu
+set -eu
 set -o pipefail
+
+SELF_PATH=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 # Add any user that is not on the system and copy their file
 # TODO: Support copying missing files and overwriting old ones
 
 # Process root first
-cp -Rv $BASE_DIR/users/root/. /root/
+cp -Rv ${SELF_PATH}/root/. /root/
 
 # Then all dev team members
-for userpath in $BASE_DIR/users/*; do
-    user=$(basename $userpath)
-    if [ $user -eq "root" ]; then continue; fi
-    if [ -d /home/$user/ ]; then
+for userpath in ${SELF_PATH}/*; do
+    # Skip non-directory, such as this file
+    if [ ! -d ${userpath} ]; then continue; fi
+
+    user=$(basename ${userpath})
+    if [ ${user} = "root" ]; then continue; fi
+    if [ -d /home/${user}/ ]; then
         echo "User '$user' home already exists, skipping"
+        continue
     fi
-    
-    adduser --shell `which zsh` --disabled-password --gecos "" --ingroup sudo $user
-    cp -Rv $userpath/. /home/$user/
-    chmod 700 /home/$user/.ssh/
+
+    echo "Adding user '$user' ($userpath)..."
+    adduser --shell `which zsh` --disabled-password --gecos "" --ingroup sudo ${user}
+    cp -Rv ${userpath}/. /home/${user}/
+    chmod 700 /home/${user}/.ssh/
 
     # Make sure to keep this the last action
-    chown -R $user:sudo /home/$user/
+    chown -R ${user}:sudo /home/${user}/
 done
 
